cb0adc126574a32739cdaaa31f41b136e3c4020d
Gamer-AI-13 <67921075+Gamer-AI-13@users.noreply.github.com>
2022-06-01 00:09:03 +0530

    test

--- otp.py
+++ otp.py
@@ -1,3 +1,3 @@
 import pyotp
-totp = pyotp.TOTP("N4HF6H5YKGSQIUCVACFHWDC2Y5OWCE5M") #Don't make it public like this
+totp = pyotp.TOTP("OAIFU5BHA7OS7EDMIZARTK233QTFCRCS") #Don't make it public like this
 print("Current OTP:", totp.now())
